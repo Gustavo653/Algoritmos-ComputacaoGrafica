bdc66cdea434b741406e806a7decd9a57135099a
Gustavo <gustavohs2004@gmail.com>
2024-03-03 20:26:12 -0300

    Atualizando nome de arquivos

---
+++ 01-03-2024@1/Program.cs
@@ -0,0 +1,11 @@
+﻿const double gravidade = 9.81;
+
+Console.WriteLine("Digite a altura da queda em metros:");
+double alturaQueda = Convert.ToDouble(Console.ReadLine());
+
+double tempoQueda = Math.Sqrt((2 * alturaQueda) / gravidade);
+double velocidadeImpacto = gravidade * tempoQueda;
+double distanciaPercorrida = 0.5 * gravidade * Math.Pow(tempoQueda, 2);
+
+Console.WriteLine("Tempo de queda: {0:F2} segundos", tempoQueda);
+Console.WriteLine("Velocidade no momento do impacto: {0:F2} m/s", velocidadeImpacto);
