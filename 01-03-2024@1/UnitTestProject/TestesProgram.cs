c9b1b51a156cf2a094f84190e4583018541b2106
Gustavo <gustavohs2004@gmail.com>
2024-03-03 21:04:04 -0300

    Adicionando testes

---
+++ 01-03-2024@1/UnitTestProject/TestesProgram.cs
@@ -0,0 +1,63 @@
+using _01_03_2024_1;
+
+namespace UnitTestProject
+{
+    [TestFixture]
+    public class TestesProgram
+    {
+        [Test]
+        public void TestCalcularStatusFinal()
+        {
+            const double gravidade = 9.81;
+            double alturaQueda = 10;
+
+            Program.CalcularStatusFinal(gravidade, alturaQueda, out double tempoQueda, out double velocidadeImpacto, out double distanciaPercorrida);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(tempoQueda, Is.EqualTo(1.4278431229270645).Within(0.0001));
+                Assert.That(velocidadeImpacto, Is.EqualTo(14.007141035914504).Within(0.0001));
+                Assert.That(distanciaPercorrida, Is.EqualTo(10.000000000000002).Within(0.0001));
+            });
+        }
+
+        [Test]
+        public void TestCalcularStatusAtual()
+        {
+            const double gravidade = 9.81;
+            double tempoQueda = 2.0248456731316584;
+            double segundo = 1;
+
+            Program.CalcularStatusAtual(gravidade, tempoQueda, segundo, out double tempoRestante, out double velocidadeInstantanea, out double distanciaPercorridaInstantanea);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(tempoRestante, Is.EqualTo(1.0248456731316584).Within(0.0001));
+                Assert.That(velocidadeInstantanea, Is.EqualTo(9.81).Within(0.0001));
+                Assert.That(distanciaPercorridaInstantanea, Is.EqualTo(4.905).Within(0.0001));
+            });
+        }
+
+        [Test]
+        public void TestExibirStatusAtual()
+        {
+            double distanciaPercorrida = 49.05;
+            double segundo = 2.0;
+            double tempoRestante = 0.0;
+            double velocidadeInstantanea = 19.62;
+            double distanciaPercorridaInstantanea = 49.05;
+
+            using StringWriter sw = new();
+            Console.SetOut(sw);
+            Program.ExibirStatusAtual(distanciaPercorrida, segundo, tempoRestante, velocidadeInstantanea, distanciaPercorridaInstantanea);
+            string expectedOutput = string.Format(
+                "\nSegundo: {0:F2}\r\nTempo Restante: {1:F2} segundos\r\nVelocidade Instantânea: {2:F2} m/s\r\nDistância Percorrida: {3:F2} metros\r\nDistância Restante: {4:F2} metros\r\n------------------------------------------\r\n",
+                segundo,
+                tempoRestante,
+                velocidadeInstantanea,
+                distanciaPercorridaInstantanea,
+                distanciaPercorrida - distanciaPercorridaInstantanea);
+            Assert.That(sw.ToString(), Is.EqualTo(expectedOutput));
+        }
+    }
+}
