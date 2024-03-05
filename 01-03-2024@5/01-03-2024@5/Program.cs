ac95fb4876e6206dd17450b974f74edc6609126e
Gustavo <gustavohs2004@gmail.com>
2024-03-05 00:11:16 -0300

    Adicionando atividade 5

---
+++ 01-03-2024@5/01-03-2024@5/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _01_03_2024_5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Bem-vindo ao calculador de Movimento Circular Uniforme!");
+
+            Console.Write("Insira o raio da trajetória circular (em metros): ");
+            double raio = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira a velocidade angular (em rad/s): ");
+            double velocidadeAngular = double.Parse(Console.ReadLine());
+
+            double velocidadeTangencial = CalcularVelocidadeTangencial(velocidadeAngular, raio);
+            double aceleracaoCentripeta = CalcularAceleracaoCentripeta(velocidadeAngular, raio);
+
+            ExibirResultados(velocidadeAngular, velocidadeTangencial, aceleracaoCentripeta);
+        }
+
+        public static double CalcularVelocidadeTangencial(double velocidadeAngular, double raio)
+        {
+            return velocidadeAngular * raio;
+        }
+
+        public static double CalcularAceleracaoCentripeta(double velocidadeAngular, double raio)
+        {
+            return Math.Pow(velocidadeAngular, 2) * raio;
+        }
+
+        public static void ExibirResultados(double velocidadeAngular, double velocidadeTangencial, double aceleracaoCentripeta)
+        {
+            Console.WriteLine("\nResultados:");
+            Console.WriteLine("Velocidade Angular: " + velocidadeAngular + " rad/s");
+            Console.WriteLine("Velocidade Tangencial: " + velocidadeTangencial + " m/s");
+            Console.WriteLine("Aceleração Centrípeta: " + aceleracaoCentripeta + " m/s^2");
+        }
+    }
+}
---
+++ 01-03-2024@5/UnitTestProject/ProgramTest.cs
@@ -0,0 +1,37 @@
+using _01_03_2024_5;
+
+namespace UnitTestProject
+{
+    public class ProgramTest
+    {
+        [Test]
+        public void TestCalcularVelocidadeTangencial()
+        {
+            // Arrange
+            double raio = 5.0;
+            double velocidadeAngular = 2.0;
+            double expectedVelocidadeTangencial = velocidadeAngular * raio;
+
+            // Act
+            double result = Program.CalcularVelocidadeTangencial(velocidadeAngular, raio);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedVelocidadeTangencial));
+        }
+
+        [Test]
+        public void TestCalcularAceleracaoCentripeta()
+        {
+            // Arrange
+            double raio = 5.0;
+            double velocidadeAngular = 2.0;
+            double expectedAceleracaoCentripeta = Math.Pow(velocidadeAngular, 2) * raio;
+
+            // Act
+            double result = Program.CalcularAceleracaoCentripeta(velocidadeAngular, raio);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(expectedAceleracaoCentripeta));
+        }
+    }
+}
